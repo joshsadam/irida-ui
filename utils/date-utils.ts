90bc83de1e58923f7b890db20fe8ea495ed90198
Josh Adam <josh.adam@canada.ca>
2022-02-02 14:55:54 -0600

    Added JSDoc

--- utils/date-utils.ts
+++ utils/date-utils.ts
@@ -1,3 +1,8 @@
+/**
+ * Formats a Date into a standardized and localized string
+ * @param {Date} date - The date to format
+ * @param {string} [locale=en-CA] - The locale to format the date to.
+ */
 export function formatTimeStamp(
   date: Date,
   locale: string | string[] = 'en-CA',
