9f0358138fd57e40df23113968000d2ceb71d861
Josh Adam <josh.adam@canada.ca>
2022-02-28 14:01:53 -0600

    Cleaned up login testing

--- app/components/__tests__/login.tsx
+++ app/components/__tests__/login.tsx
@@ -3,26 +3,36 @@ import Login from '../login';
 
 test('Renders login form', async () => {
   render(<Login />);
-  const username = screen.getByLabelText(/username/i);
-  const password = screen.getByLabelText(/password/i);
-  const submit = screen.getByText(/sign in$/i);
+  testBasicSetup();
 
-  expect(username).toHaveAttribute('name', 'username');
-  expect(username).toHaveAttribute('required');
-  expect(password).toHaveAttribute('name', 'password');
-  expect(password).toHaveAttribute('required');
+  // Ensure that the error is not displayed
+  const loginError = screen.queryByRole('alert');
+  expect(loginError).toBeNull();
 });
 
 test('Renders login form with error', async () => {
   render(<Login errors={true} />);
+  testBasicSetup();
+
+  // Ensure that the error is displayed
+  screen.getByRole('alert');
+});
+
+test('Login form submit button should be disabled when busy', async () => {
+  render(<Login busy={true} />);
+  testBasicSetup();
+
+  const button = screen.getByText(/sign in$/i);
+  expect(button).toBeDisabled();
+});
+
+function testBasicSetup() {
   const username = screen.getByLabelText(/username/i);
   const password = screen.getByLabelText(/password/i);
-  const submit = screen.getByText(/sign in$/i);
+  screen.getByText(/sign in$/i);
 
   expect(username).toHaveAttribute('name', 'username');
   expect(username).toHaveAttribute('required');
   expect(password).toHaveAttribute('name', 'password');
   expect(password).toHaveAttribute('required');
-
-  const alert = screen.getAllByRole('alert');
-});
+}
