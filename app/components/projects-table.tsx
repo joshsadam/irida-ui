9b0f00ad54496aa2b8b29376d20b109a710486ca
Josh Adam <josh.adam@canada.ca>
2022-02-28 14:35:12 -0600

    Made the projects table it's own component

---
+++ app/components/projects-table.tsx
@@ -0,0 +1,45 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Link } from 'remix';
+import { Project } from '~/types';
+
+type ProjectsTableProps = {
+  projects: Project[];
+};
+
+const columns: GridColDef[] = [
+  {
+    field: 'name',
+    headerName: 'Name',
+    width: 250,
+    renderCell: ({ row }) => {
+      return <Link to={`/projects/${row.id}`}>{row.name}</Link>;
+    },
+  },
+  {
+    field: 'projectDescription',
+    headerName: 'Description',
+    width: 400,
+  },
+  {
+    field: 'createdDate',
+    headerName: 'Created',
+    type: 'dateTime',
+    width: 250,
+    valueGetter: ({ value }) => value && new Date(value),
+  },
+];
+
+export function ProjectsTable({ projects }: ProjectsTableProps) {
+  return (
+    <div style={{ display: 'flex', height: '100%' }}>
+      <div style={{ flexGrow: 1 }}>
+        <DataGrid
+          rows={projects}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+        />
+      </div>
+    </div>
+  );
+}
--- app/routes/__app/projects/index.tsx
+++ app/routes/__app/projects/index.tsx
@@ -2,9 +2,7 @@ import { gql } from '@apollo/client';
 import { AddOutlined } from '@mui/icons-material';
 import { Button, Paper, Stack } from '@mui/material';
 import Box from '@mui/material/Box';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import {
-  Link,
   LoaderFunction,
   MetaFunction,
   redirect,
@@ -12,11 +10,11 @@ import {
   useNavigate,
 } from 'remix';
 import { Token } from 'simple-oauth2';
+import { ProjectsTable } from '~/components/projects-table';
 import Title from '~/components/Title';
 import client from '~/services/apollo-client';
 import { authenticator } from '~/services/auth';
 import { Project } from '~/types';
-import { formatTimeStamp } from '~/utils/date-utils';
 
 const ALL_PROJECTS_QUERY = gql`
   query ALL_PROJECTS_QUERY {
@@ -67,29 +65,6 @@ export default function Projects() {
   const navigate = useNavigate();
   const projects = useLoaderData<Project[]>();
 
-  const columns: GridColDef[] = [
-    {
-      field: 'name',
-      headerName: 'Name',
-      width: 250,
-      renderCell: ({ row }) => {
-        return <Link to={`/projects/${row.id}`}>{row.name}</Link>;
-      },
-    },
-    {
-      field: 'projectDescription',
-      headerName: 'Description',
-      width: 300,
-    },
-    {
-      field: 'createdDate',
-      headerName: 'Created',
-      type: 'dateTime',
-      width: 250,
-      valueGetter: ({ value }) => value && new Date(value),
-    },
-  ];
-
   return (
     <>
       <Stack
@@ -110,16 +85,7 @@ export default function Projects() {
       </Stack>
       <Paper>
         <Box sx={{ p: 2, height: '800px' }}>
-          <div style={{ display: 'flex', height: '100%' }}>
-            <div style={{ flexGrow: 1 }}>
-              <DataGrid
-                rows={projects}
-                columns={columns}
-                pageSize={10}
-                rowsPerPageOptions={[10]}
-              />
-            </div>
-          </div>
+          <ProjectsTable projects={projects} />
         </Box>
       </Paper>
     </>
